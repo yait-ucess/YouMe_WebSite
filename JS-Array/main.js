10bf931a9c40c2e4839eea3b98d8edcd8e52523d
yait-ucess <bandai28@icloud.com>
2021-03-23 23:56:52 +0900

    shift, pop
    shiftプロパティで配列の最初の要素を削除できる
    popプロパティで配列の最後の要素を削除できる

--- JS-Array/main.js
+++ JS-Array/main.js
@@ -1,3 +1,4 @@
-const array = [1,2,3,4,5,6, 'banana', false];
-array.unshift('new item')
-console.log(array);
+const arry = [1,2,3,4,5,6, 'banana', false];
+arry.unshift('new item')
+const val = arry.shift();
+console.log(val);
