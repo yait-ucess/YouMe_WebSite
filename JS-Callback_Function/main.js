41906e2cef811551d7b9bfb2635f868f46a8712a
yait-ucess <bandai28@icloud.com>
2021-03-25 23:08:46 +0900

    callback-function
    コールバック関数

--- JS-Callback_Function/main.js
+++ JS-Callback_Function/main.js
@@ -1,9 +1,34 @@
-function hello(callback) {
-  console.log('hello' + callback());
-}
+// function hello(callback, lastname) {
+//   console.log(callback);
+//   console.log('hello' + callback(lastname));
+// }
 
-function getName() {
-  return 'Shun';
-}
+// function getName() {
+//   return 'Shun';
+// }
 
-hello(getName);
+// hello(function(name) {
+//   return 'Sato' + name;
+// }, 'Shun');
+
+// function doSomething(a, b, callback) {
+//   const result = callback(a, b);
+//   console.log(result);
+// }
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// function plus(a, b) {
+//   return a + b;
+// }
+
+// doSomething(3, 4, multiply); 
+// doSomething(11, 4, plus); 
+
+const arry = [1,2,3,4,5];
+
+for(let i = 0; i < arry.length; i++) {
+  console.log(arry[i]);
+}
