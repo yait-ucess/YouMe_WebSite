982264d1dd057117bcc2045e23af2570b88218c2
yait-ucess <bandai28@icloud.com>
2021-03-28 16:44:32 +0900

    reduce
    reduceメソッド

--- JS-Reduce/main.js
+++ JS-Reduce/main.js
@@ -1,5 +1,29 @@
 const arry = [1,2,3,4,5];
 
-arry.reduce(function(accu, curr) {
+// arry.reduce(function(accu, curr) {
+//   console.log(accu, curr);
+//   return accu * curr;
+// }, 10);
+
+
+
+const str = 'animation';
+const strArry = str.split('');
+
+function tag(accu, curr) {
   console.log(accu, curr);
-})
+  return `${accu}<${curr}>`;
+}
+
+function reduce(arry, callback, defaultValue) {
+  let accu = defaultValue;
+  for(let i = 0; i < arry.length; i++) {
+    let curr = arry[i];
+    accu = callback(accu, curr);
+  }
+
+  return accu;
+}
+
+const result = reduce(strArry, tag, "");
+console.log(result);
