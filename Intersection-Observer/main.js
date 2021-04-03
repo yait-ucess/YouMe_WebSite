bf9436f3eaecccffde0130dd442813b919d5aa94
yait-ucess <bandai28@icloud.com>
2021-04-03 23:05:46 +0900

    isIntersecting
    要素がインしたときはinview, 画面外にアウトしたときはout viewを表示

--- Intersection-Observer/main.js
+++ Intersection-Observer/main.js
@@ -1,6 +1,15 @@
 const child = document.querySelector('.child');
-const cb = function() {
-  alert('intersecting');
+const cb = function(entries, observer) {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      console.log('inview');
+    } else {
+      console.log('out view');
+    }
+  });
+  //alert('intersecting');
 }
 const io = new IntersectionObserver(cb);
 io.observe(child);
+// io.observe(child1);
+// io.observe(child2);
