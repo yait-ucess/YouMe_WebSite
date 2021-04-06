ea0e2f8bf3b7e343bf0ca6800f9c33a7a47d7d78
yait-ucess <bandai28@icloud.com>
2021-04-06 16:35:54 +0900

    IntersectionObserver
    root
    rootMargin
    threshold

--- JavaScript-Practice/Intersection-Observer/main.js
+++ JavaScript-Practice/Intersection-Observer/main.js
@@ -2,19 +2,16 @@ const child = document.querySelector('.child');
 const cb = function(entries, observer) {
   entries.forEach(entry => {
     if(entry.isIntersecting) {
-      console.log('inview');
       entry.target.classList.add('inview');
-      //observer.unobserve(entry.target);
     } else {
-      console.log('out view');
       entry.target.classList.remove('inview');
     }
   });
-  //alert('intersecting');
 }
 const options = {
   root: null,
-  rootMargin: "-500px 0px 0px 0px",
+  rootMargin: "-300px 0px",
+  threshold: 0
 };
 const io = new IntersectionObserver(cb, options);
 io.observe(child);
