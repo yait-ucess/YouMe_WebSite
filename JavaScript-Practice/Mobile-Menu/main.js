8735fd80dc6ded7fd536ed20634ccd9dedafec45
yait-ucess <bandai28@icloud.com>
2021-04-08 10:00:56 +0900

    ontouchstart
    window.ontouchstart ? 'touchstart' : 'click';
    ontouchstartが存在する場合はtouchstart, 存在しない場合はclickを発火させる
    スマホではtouchstart, PCではclickがあらかじめ存在する

--- JavaScript-Practice/Mobile-Menu/main.js
+++ JavaScript-Practice/Mobile-Menu/main.js
@@ -2,6 +2,18 @@ class MobileMenu {
   constructor () {
     this.DOM = {};
     this.DOM.btn = document.querySelector('.mobile-menu__btn');
-    
+    this.DOM.container = document.querySelector('#global-container');
+    this.eventType = window.ontouchstart ? 'touchstart' : 'click';
+    this._addEvent();
+  }
+
+  _toggle() {
+    this.DOM.container.classList.toggle('menu-open');
+  }
+
+  _addEvent() {
+    this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
   }
 }
+
+new MobileMenu();
