20da7936194585bff8b86323f056a03e89b18f68
yait-ucess <bandai28@icloud.com>
2021-04-15 21:26:13 +0900

    centerSlides: true
    スライダーが中央揃えになります

--- JavaScript-Practice/Swiper.JS/main.js
+++ JavaScript-Practice/Swiper.JS/main.js
@@ -1,7 +1,16 @@
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  //direction: 'vertical',
-  loop: true,
-  //effect: 'fade',
-  effect: 'coverflow',
-})
+document.addEventListener('DOMContentLoaded', function() {
+  const hero = new HeroSlider();
+});
+
+class HeroSlider {
+  constructor() {
+    this.swiper = new Swiper('.swiper-container', {
+      // Optional parameters
+      //direction: 'vertical',
+      loop: true,
+      //effect: 'fade',
+      effect: 'coverflow',
+      centerSlides: true,
+    })
+  }
+}
