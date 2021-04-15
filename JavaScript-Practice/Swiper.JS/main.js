cd28e12702b4bebe483d3252cae3844503dbae2f
yait-ucess <bandai28@icloud.com>
2021-04-15 22:09:51 +0900

    Swiper.JS
    様々なアニメーションをつけることができる

--- JavaScript-Practice/Swiper.JS/main.js
+++ JavaScript-Practice/Swiper.JS/main.js
@@ -1,28 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const hero = new HeroSlider();
+  const hero = new HeroSlider('.swiper-container');
+  hero.start();
 });
-
-class HeroSlider {
-  constructor() {
-    this.swiper = new Swiper('.swiper-container', {
-      // Optional parameters
-      //direction: 'vertical',
-      loop: true,
-      //effect: 'fade',
-      grabCursor: true,
-      effect: 'coverflow',
-      centerSlides: true,
-      slidesPerView: 1,
-      speed: 1000,
-      breakpoints: {
-        1024: {
-          slidesPerView: 2
-        }
-      },
-      autoplay: {
-        deray: 4000,
-        disableOnInteraction: false
-      }
-    })
-  }
-}
