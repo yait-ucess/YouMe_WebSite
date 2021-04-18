151732a187d378f1207bcf96148c30f397efc113
yait-ucess <bandai28@icloud.com>
2021-04-18 15:00:00 +0900

    部品のインポート
    @importでmodulesをインポートしてくる

--- WebSite/scripts/main.js
+++ WebSite/scripts/main.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   const hero = new HeroSlider('.swiper-container');
   hero.start();
+
+  const cb = function (el, isIntersecting) {
+    if(isIntersecting) {
+        const ta = new TweenTextAnimation(el);
+        ta.animate();
+    }
+  }
+
+  const so = new ScrollObserver('.tween-animate-title', cb);
 });
