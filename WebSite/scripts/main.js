f36e55f653fbd1e34de28ecbb8e93e6f6d120ecf
yait-ucess <bandai28@icloud.com>
2021-04-22 18:47:49 +0900

    setter/getter
    インスタンス変数をクラス内から参照するメソッドが「getter」です。
    インスタンス変数をクラス内で更新するメソッドが「setter」です。
    
    setter/getterを使って値を設定する変数は、
    基本的に＿が先頭についている場合が多いので
    ＿が先頭についている変数を見つけた場合には、
    そのclass内で getter/setter メソッドがあるかどうかを確認した方が良いかと思います。

--- WebSite/scripts/main.js
+++ WebSite/scripts/main.js
@@ -1,46 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const hero = new HeroSlider('.swiper-container');
-  hero.start();
-
-  // const cb = function (el, inview) {
-  //   if(inview) {
-  //       const ta = new TweenTextAnimation(el);
-  //       ta.animate();
-  //   }
-  // }
-
-  // const so = new ScrollObserver('.tween-animate-title', cb);
-
-  // const _inviewAnimation = function(el, inview) {
-  //   if(inview) {
-  //     el.classList.add('inview');
-  //   } else {
-  //     el.classList.remove('inview');
-  //   }
-  // }
-
-  // const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
-
-  //const header = document.querySelector('.header');
-  // const _navAnimation = function(el, inview) {
-  //   if(inview) {
-  //     header.classList.remove('triggered');
-  //   } else {
-  //     header.classList.add('triggered');
-  //   }
-  // }
-
-  //const so3 = new ScrollObserver('.nav-trigger', _navAnimation, {once: false});
-
-  new MobileMenu();
-
-  new Main();
+  const main = new Main();
 });
 
 class Main {
   constructor() {
     this.header = document.querySelector('.header');
     this._observer = [];
+    this._init();
+  }
+
+  set observers(val) {
+    this._observer.push(val);
+  }
+
+  get observers() {
+    return this._observer;
+  }
+
+  _init() {
+    new MobileMenu();
+    this.hero = new HeroSlider('.swiper-container');
     this._scrollInit();
   }
 
@@ -67,13 +46,29 @@ class Main {
     }
   }
 
+  _toggleSlideAnimation(el, inview) {
+    if(inview) {
+      this.hero.start();
+    } else {
+      this.hero.stop();
+    }
+  }
+
+  _destroyObservers() {
+    this.observers.forEach(ob => {
+      ob.destroy();
+    });
+  }
+
+  destroy() {
+    this._destroyObservers();
+  }
+
   _scrollInit() {
-    this._observer.push(
-      new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), {once: false})      
-    );
-    this._observer.push(
-      new ScrollObserver('.cover-slide', this._inviewAnimation)
-    );
-    const so = new ScrollObserver('.tween-animate-title', this._textAnimation);
+    this.observers = new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), {once: false});
+    this.observers = new ScrollObserver('.cover-slide', this._inviewAnimation);
+    this.observers = new ScrollObserver('.tween-animate-title', this._textAnimation);
+    this.observers = new ScrollObserver('.swiper-container', this._toggleSlideAnimation.bind(this), {once: false});
+    console.log(this.observers);
   }
 }
